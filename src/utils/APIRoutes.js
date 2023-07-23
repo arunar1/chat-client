7d76d29b19574073a029bd6c51ce0bd5ae934daa
arunar1 <arunarofficial00@gmail.com>
2023-07-23 18:06:04 +0530

    commit

--- src/utils/APIRoutes.js
+++ src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "http://localhost:5000";
+export const host = "https://vercel.com/arunar1/chat-server/CbpVjEErsEhBdV8xyMHEJF3KDaE8";
 export const loginRoute = `${host}/api/auth/login`;
 export const registerRoute = `${host}/api/auth/register`;
 export const logoutRoute = `${host}/api/auth/logout`;
