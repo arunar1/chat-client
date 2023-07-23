5785055297442e2832a40de903f93d7fe45c612d
arunar1 <arunarofficial00@gmail.com>
2023-07-23 18:20:24 +0530

    commit

--- src/utils/APIRoutes.js
+++ src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "https://vercel.com/arunar1/chat-server/CbpVjEErsEhBdV8xyMHEJF3KDaE8";
+export const host = "https://chat-server-wheat-delta.vercel.app";
 export const loginRoute = `${host}/api/auth/login`;
 export const registerRoute = `${host}/api/auth/register`;
 export const logoutRoute = `${host}/api/auth/logout`;
